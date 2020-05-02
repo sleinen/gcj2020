9d915c3a97fe31890d54dd30f594ca9d1e9368bb
Simon Leinen (SWITCH) <simon.leinen@switch.ch>
2020-05-02 15:34:45 +0200

    Simplify FIND-ZERO

--- 1c/2.lisp
+++ 1c/2.lisp
@@ -2,40 +2,31 @@
 ;;
 
 (defun solve (&optional (in *standard-input*))
-  (dotimes (caseno (the (integer 0 1000) (read in)))
-    (format t "Case #~D: ~A~&" (+ caseno 1) (solve-case in))))
+  (dotimes (caseno (read in))
+    (format t "Case #~D: ~A~%" (+ caseno 1) (solve-case in))))
 
 (defun solve-case (in)
-  (let ((u (read in)))
-    (let ((pairs (make-array (list 10000))))
-      (let ((charset '()))
-        (dotimes (i 10000)
-          (let ((qi (read in)) (ri (string-trim '(#\Space #\Tab #\Newline) (read-line in))))
-            (dotimes (k (length ri))
-              (unless (member (aref ri k) charset)
-                (push (aref ri k) charset)))
-            (setf (aref pairs i) (cons qi ri))))
-        (solve-case-1 u pairs charset)))))
-
-(defun find-zero (u pairs charset)
-  (declare (ignore u))
-  (let ((zero-candidates charset))
+  (let ((u (read in))
+        (pairs (make-array (list 10000)))
+        (charset '()))
     (dotimes (i 10000)
-      (let ((first-digit (aref (cdr (aref pairs i)) 0)))
-        (when (member first-digit zero-candidates)
-          (setq zero-candidates
-                (set-difference zero-candidates (list first-digit))))))
-    ;; This can only happen when there are no zeroes in the dataset.
-    ;; Highly unlikely!
-    (when (endp zero-candidates)
-      (return-from find-zero (first charset)))
-    ;; This can happen when a non-zero digit fails to occur as a leading digit.
-    ;; Also highly unlikely!
-    ;; (unless (endp (rest zero-candidates))
-    ;;   (return-from find-zero (first charset)))
-    (first zero-candidates)))
+      (let ((qi (read in))
+            (ri (string-trim '(#\Space #\Tab #\Newline) (read-line in))))
+        (dotimes (k (length ri))
+          (unless (member (aref ri k) charset)
+            (push (aref ri k) charset)))
+        (setf (aref pairs i) (cons qi ri))))
+    (solve-case-1 u pairs charset)))
+
+(defun find-zero (pairs charset)
+  (first (set-difference
+          charset
+          (reduce #'union
+                  (map 'list #'(lambda (pair) (list (aref (cdr pair) 0)))
+                       pairs)))))
 
 (defun solve-case-1 (u pairs charset)
+  (declare (ignore u))
   (when (/= (length charset) 10)
     (return-from solve-case-1 "CHARSETERR"))
   (unless (every #'(lambda (c) (and (alpha-char-p c) (upper-case-p c))) charset)
@@ -43,31 +34,21 @@
   (unless (every #'(lambda (pair) (> (length (cdr pair)) 0)) pairs)
     (return-from solve-case-1  "EMPTYSTR"))
 
-  (let ((zero (find-zero u pairs charset)))
-    ;; Pad all Qi with leading zeroes
-    (dotimes (i 10000)
-      (let ((pair (aref pairs i)))
-        (when (< (length (cdr pair)) u)
-          (setf (cdr pair)
-                (concatenate 'string
-                             (make-string (- u (length (cdr pair)))
-                                          :initial-element zero)
-                             (cdr pair)))))))
+  (let ((zero (find-zero pairs charset)))
 
-  ;; Now count leading digits
-  (let ((counts (make-array (list 10) :initial-element 0)))
-    (dotimes (i 10000)
-      (let ((pair (aref pairs i)))
-        (let ((ri (cdr pair)))
-          (dotimes (k 1)
-            (incf (aref counts (position (aref ri k) charset)))))))
+    ;; Now count leading digits
+    (let ((counts (make-array (list 10) :initial-element 0)))
+      (dotimes (i 10000)
+        (let ((ri (cdr (aref pairs i))))
+          (incf (aref counts (position (aref ri 0) charset)))))
 
-    (let ((count-to-digit (pairlis (coerce counts 'list) charset)))
-      (let ((sorted-counts (sort counts #'>)))
-        (let ((result (make-string 10)))
-          (dotimes (i 10)
-            (setf (aref result i)
-                  (cdr (assoc (aref sorted-counts i) count-to-digit))))
-          result)))))
+      (let ((count-to-digit (pairlis (coerce counts 'list) charset)))
+        (let ((sorted-counts (sort counts #'>)))
+          (let ((result (make-string 10)))
+            (setf (aref result 0) zero)
+            (dotimes (i 9)
+              (setf (aref result (1+ i))
+                    (cdr (assoc (aref sorted-counts i) count-to-digit))))
+            result))))))
 
 (solve)
