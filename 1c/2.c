a18d7b72a6fe45713fe3764eab920a72b6c177d9
Simon Leinen (SWITCH) <simon.leinen@switch.ch>
2020-05-03 12:12:21 +0200

    Problem 2: Improve(?) C version

--- 1c/2.c
+++ 1c/2.c
@@ -3,13 +3,7 @@
 #include <stdlib.h>
 #include <memory.h>
 
-static int _desc (const uint32_t *x, const uint32_t *y) { return *y - *x; }
-static int desc (const void *x, const void *y) { return _desc(x, y); }
-
-static int pos(uint32_t val, uint32_t *v, size_t count) {
-  for (int k = 0; k < count; ++k) { if (v[k] == val) return k; }
-  return -1;
-}
+static int desc (const uint32_t *x, const uint32_t *y) { return *y - *x; }
 
 int
 main () {
@@ -19,30 +13,26 @@ main () {
   unsigned i, k;
   scanf ("%u\n", &T);
   for (unsigned caseno = 1; caseno <= T; ++caseno) {
-    uint64_t seen = 0;
-    uint32_t counts[26] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    uint32_t seen = 0;
+    uint32_t counts[26] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+      scounts[26], *it;
+    printf ("Case #%d: ", caseno);
     scanf ("%u\n", &U);
-    char result[11];
-    result[10] = 0;
     for (k = 0; k < 10000; ++k) {
       scanf ("%lld %s\n", &Qi, Ri);
-      for (i = 0, cp = Ri; *cp; ++i, ++cp) {
-        if (i == 0) {
-          ++counts[*cp-'A'];
-        };
-        seen |= (1 << (*cp-'A'));
-      }
+      ++counts[Ri[0]-'A'];
+      for (i = 0, cp = Ri; *cp; ++i, ++cp) seen |= (1 << (*cp-'A'));
     }
-    for (k = 0; k < 26; ++k) {
+    for (k = 0; k < 26; ++k)
       if ((seen & (1 << k)) && counts[k] == 0) {
-        result[0] = k + 'A';
+        putchar (k + 'A'); break;
       }
-    }
-    uint32_t scounts[26], *it;
-    memcpy (scounts, counts, sizeof (uint32_t) * 26);
-    qsort (scounts, 26, sizeof (uint32_t), desc);
+    memcpy (scounts, counts, sizeof counts[0] * 26);
+    qsort (scounts, 26, sizeof scounts[0],
+           (int (*) (const void *, const void *)) desc);
     for (it = scounts, i = 0; i < 9; ++it, ++i)
-      result[i+1] = pos(*it, counts, 26) + 'A';
-    printf ("Case #%d: %s\n", caseno, result);
+      for (int k = 0; k < 26; ++k)
+        if (counts[k] == scounts[i]) putchar(k + 'A');
+    putchar('\n');
   }
 }
