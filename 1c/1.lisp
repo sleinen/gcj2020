ad79a2a9d9762a56a0f486b4fdda9e42da7b2b58
Simon Leinen (SWITCH) <simon.leinen@switch.ch>
2020-05-03 11:38:00 +0200

    Problem 1: Eliminate outer function

--- 1c/1.lisp
+++ 1c/1.lisp
@@ -1,18 +1,15 @@
 ;;; Google Code Jam 2020, Round 1C, Problem 1: Overexcited Fan
 ;;
-(defun solve (&optional (in *standard-input*))
-  (dotimes (caseno (read in))
+(labels
+    ((solve (x y seq steps)
+       (cond ((<= (+ (abs x) (abs y)) steps) steps)
+             ((= steps (length seq)) 'IMPOSSIBLE)
+             (t (solve
+                 (+ x (case (char seq steps) (#\W -1) (#\E 1) (otherwise 0)))
+                 (+ y (case (char seq steps) (#\N 1) (#\S -1) (otherwise 0)))
+                 seq
+                 (1+ steps))))))
+  (dotimes (caseno (read))
     (format t "Case #~D: ~A~%"
             (+ caseno 1)
-            (solve-case-1 (read in) (read in) (read-line in) 0))))
-
-(defun solve-case-1 (x y seq steps)
-  (cond ((<= (+ (abs x) (abs y)) steps) steps)
-        ((= steps (length seq)) 'IMPOSSIBLE)
-        (t (solve-case-1
-            (+ x (case (char seq steps) (#\W -1) (#\E 1) (otherwise 0)))
-            (+ y (case (char seq steps) (#\N 1) (#\S -1) (otherwise 0)))
-            seq
-            (1+ steps)))))
-
-(solve)
+            (solve (read) (read) (read-line) 0))))
