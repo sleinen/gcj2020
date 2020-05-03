c22967fd7cff55e0ab81a8d33047395951405be8
Simon Leinen (SWITCH) <simon.leinen@switch.ch>
2020-05-03 11:29:44 +0200

    More pointless simplification

--- 1c/1.lisp
+++ 1c/1.lisp
@@ -1,22 +1,19 @@
 ;;; Google Code Jam 2020, Round 1C, Problem 1: Overexcited Fan
 ;;
-
 (defun solve (&optional (in *standard-input*))
-  (dotimes (caseno (the (integer 0 1000) (read in)))
-    (format t "Case #~D: ~A~%" (+ caseno 1) (solve-case in))))
-
-(defun solve-case (in)
-  (solve-case-1 (read in) (read in) (coerce (read-line in) 'list) 0))
+  (dotimes (caseno (read in))
+    (format t "Case #~D: ~A~%"
+            (+ caseno 1)
+            (solve-case-1
+             (read in) (read in) (coerce (read-line in) 'list) 0))))
 
 (defun solve-case-1 (x y seq steps)
-  (labels
-      ((dir-xy-offset (c)
-         (values (ecase c ((#\N) 0) ((#\S)  0) ((#\W) -1) ((#\E) 1))
-                 (ecase c ((#\N) 1) ((#\S) -1) ((#\W)  0) ((#\E) 0)))))
-    (cond ((<= (+ (abs x) (abs y)) steps) steps)
-          ((endp seq) 'IMPOSSIBLE)
-          (t (multiple-value-bind (dx dy)
-                 (dir-xy-offset (first seq))
-               (solve-case-1 (+ x dx) (+ y dy) (rest seq) (1+ steps)))))))
+  (cond ((<= (+ (abs x) (abs y)) steps) steps)
+        ((endp seq) 'IMPOSSIBLE)
+        (t (solve-case-1
+            (+ x (case (first seq) (#\W -1) (#\E 1) (otherwise 0)))
+            (+ y (case (first seq) (#\N 1) (#\S -1) (otherwise 0)))
+            (rest seq)
+            (1+ steps)))))
 
 (solve)
