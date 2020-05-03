3b7bfa08ad833bc9481dcbc4e8301acbb5fd87ba
Simon Leinen (SWITCH) <simon.leinen@switch.ch>
2020-05-03 11:34:39 +0200

    Problem 1: Eliminate string/list conversion

--- 1c/1.lisp
+++ 1c/1.lisp
@@ -4,16 +4,15 @@
   (dotimes (caseno (read in))
     (format t "Case #~D: ~A~%"
             (+ caseno 1)
-            (solve-case-1
-             (read in) (read in) (coerce (read-line in) 'list) 0))))
+            (solve-case-1 (read in) (read in) (read-line in) 0))))
 
 (defun solve-case-1 (x y seq steps)
   (cond ((<= (+ (abs x) (abs y)) steps) steps)
-        ((endp seq) 'IMPOSSIBLE)
+        ((= steps (length seq)) 'IMPOSSIBLE)
         (t (solve-case-1
-            (+ x (case (first seq) (#\W -1) (#\E 1) (otherwise 0)))
-            (+ y (case (first seq) (#\N 1) (#\S -1) (otherwise 0)))
-            (rest seq)
+            (+ x (case (char seq steps) (#\W -1) (#\E 1) (otherwise 0)))
+            (+ y (case (char seq steps) (#\N 1) (#\S -1) (otherwise 0)))
+            seq
             (1+ steps)))))
 
 (solve)
